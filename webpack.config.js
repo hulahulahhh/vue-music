5afb84300763868b806f2d836f1b82565cc674c5
hulahulahhh <1002340239@qq.com>
2019-09-25 11:36:48 +0800

    feat: 引入清除插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
@@ -20,7 +21,8 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: "babel-loader",
+        exclude: /node_modules/ // why? 
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -31,18 +33,20 @@ module.exports = {
         use: ["vue-style-loader", "css-loader", "postcss-loader", "sass-loader"]
       },
       {
-        // style-loader用处？  
+        // style-loader用处？
         test: /\.css$/,
         use: ["css-loader"]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     // 一定要引入这个插件，不然vue-loader无法生效
     // 这个插件的作用是，将你定义的其他规则复制并应用到vue文件中其他语言块（style块、script块）
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html"
     })
-  ]
+  ],
+  stats: 'minimal'
 };
